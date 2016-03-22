dc95d279833bad9471eda58d22fa2f4c4c67378d
Christopher D. Canfield <cdcanfield@gmail.com>
2016-03-21 21:52:16 -0400

    Continued adding camera controls to multi viewports prototype

--- prototypes/multiple-viewports/src/com/christopherdcanfield/viewport/MultipleViewportsApp.java
+++ prototypes/multiple-viewports/src/com/christopherdcanfield/viewport/MultipleViewportsApp.java
@@ -8,9 +8,11 @@ import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 
 public class MultipleViewportsApp extends SimpleApplication
@@ -23,9 +25,16 @@ public class MultipleViewportsApp extends SimpleApplication
 
 	private Camera camera2;
 
+	private boolean moveCameraEast;
+	private boolean moveCameraWest;
+	private boolean moveCameraNorth;
+	private boolean moveCameraSouth;
+
 	@Override
 	public void simpleInitApp()
 	{
+		flyCam.setEnabled(false);
+
 		camera2 = cam.clone();
 		camera2.setViewPort(0.85f, 0.98f, 0.85f, 0.98f);
 		ViewPort viewport = renderManager.createPostView("camera2", camera2);
@@ -41,9 +50,41 @@ public class MultipleViewportsApp extends SimpleApplication
         geom.setMaterial(mat);                   // set the cube's material
         rootNode.attachChild(geom);              // make the cube appear in the scene
 
-        inputManager.addMapping("move-camera-right", new KeyTrigger(KeyInput.KEY_RIGHT));
+        inputManager.addMapping("move-camera-east", new KeyTrigger(KeyInput.KEY_RIGHT));
+        inputManager.addMapping("move-camera-west", new KeyTrigger(KeyInput.KEY_LEFT));
+        inputManager.addMapping("move-camera-north", new KeyTrigger(KeyInput.KEY_UP));
+        inputManager.addMapping("move-camera-south", new KeyTrigger(KeyInput.KEY_DOWN));
         inputManager.addListener((ActionListener)(String name, boolean isPressed, float tpf) -> {
+        	if (name.equals("move-camera-east")) {
+	        	moveCameraEast = isPressed;
+        	} else if (name.equals("move-camera-west")) {
+        		moveCameraWest = isPressed;
+        	} else if (name.equals("move-camera-north")) {
+        		moveCameraNorth = isPressed;
+        	} else if (name.equals("move-camera-south")) {
+        		moveCameraSouth = isPressed;
+        	}
+        }, "move-camera-east", "move-camera-west", "move-camera-north", "move-camera-south");
+	}
 
-        }, "move-camera-right");
+	@Override
+	public void simpleUpdate(float tpf)
+	{
+		if (moveCameraEast) {
+			Vector3f newCameraPosition = cam.getLocation().add(0.01f, 0, 0);
+        	cam.setLocation(newCameraPosition);
+		}
+		else if (moveCameraWest) {
+			Vector3f newCameraPosition = cam.getLocation().add(-0.01f, 0, 0);
+        	cam.setLocation(newCameraPosition);
+		}
+		else if (moveCameraNorth) {
+			Vector3f newCameraPosition = cam.getLocation().add(0, 0.01f, 0);
+        	cam.setLocation(newCameraPosition);
+		}
+		else if (moveCameraSouth) {
+			Vector3f newCameraPosition = cam.getLocation().add(0, -0.01f, 0);
+        	cam.setLocation(newCameraPosition);
+		}
 	}
 }
